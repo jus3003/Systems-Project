83774c2e6253ba59f35881ab46d3622225d2a0f2
Jordan French <jordanfrench@utexas.edu>
2023-11-16 14:15:05 -0600

    deltas

--- agent_env_monthly.py
+++ agent_env_monthly.py
@@ -17,27 +17,27 @@ def commute_update(neighbourhood, population):
     # cars: all scores effected
     car_delta = neighbourhood.monthly_utilization[0] / population - 1/12
     car_baseline = neighbourhood.baseline_scores[0]
-    car_weights = [0.25, 0.25, 0.2, 0.2]
+    car_weights = [5, 5, 4, 4]
 
     neighbourhood.commutescores[0] = calculate_score(car_delta, car_baseline, car_weights)
 
     # bus: mild impacts on convenience and speed
     bus_delta = neighbourhood.monthly_utilization[1] / population - 1/12
     bus_baseline = neighbourhood.baseline_scores[1]
-    bus_weights = [0.15, 0.15, 0, 0]
+    bus_weights = [3, 3, 0, 0]
 
     neighbourhood.commutescores[1] = calculate_score(bus_delta, bus_baseline, bus_weights) 
 
     # bike: very mild impacts on convenience
     bike_delta = neighbourhood.monthly_utilization[2] / population - 1/12
     bike_baseline =neighbourhood.baseline_scores[2]
-    bike_weights = [0.1, 0, 0, 0]
+    bike_weights = [2, 0, 0, 0]
     neighbourhood.commutescores[2] = calculate_score(bike_delta, bike_baseline, bike_weights)
 
     # walk: very mild impacts on convenience
     walk_delta = neighbourhood.monthly_utilization[3] / population - 1/12
     walk_baseline = neighbourhood.baseline_scores[3]
-    walk_weights = [0.05, 0, 0, 0]
+    walk_weights = [1, 0, 0, 0]
     neighbourhood.commutescores[3] = calculate_score(walk_delta, walk_baseline, walk_weights)
 
 def calculate_score(deltas, baseline, weights):
@@ -47,10 +47,10 @@ def calculate_score(deltas, baseline, weights):
     deltas = np.array(deltas)
 
     # calculate the score for each element
-    scores = baseline + weights * deltas
+    scores = baseline - weights * deltas
 
     # clip the scores
     scores = np.clip(scores, 0, 10)
-    print(scores)
+    print(scores[1])
 
     return scores
