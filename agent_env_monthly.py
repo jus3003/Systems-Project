4dad1a120c12e6f4a77465552bf7e98cebbecfed
Jordan French <jordanfrench@utexas.edu>
2023-11-16 14:08:49 -0600

    commute updates

--- agent_env_monthly.py
+++ agent_env_monthly.py
@@ -15,44 +15,42 @@ def commute_update(neighbourhood, population):
     # congestion effects
 
     # cars: all scores effected
-    car_ut = neighbourhood.monthly_utilization[0] / population
+    car_delta = neighbourhood.monthly_utilization[0] / population - 1/12
     car_baseline = neighbourhood.baseline_scores[0]
     car_weights = [0.25, 0.25, 0.2, 0.2]
 
-    neighbourhood.commutescores[0] = calculate_score(car_ut, car_baseline, car_weights)
+    neighbourhood.commutescores[0] = calculate_score(car_delta, car_baseline, car_weights)
 
     # bus: mild impacts on convenience and speed
-    bus_ut = neighbourhood.monthly_utilization[1] / population
+    bus_delta = neighbourhood.monthly_utilization[1] / population - 1/12
     bus_baseline = neighbourhood.baseline_scores[1]
     bus_weights = [0.15, 0.15, 0, 0]
 
-    neighbourhood.commutescores[1] = calculate_score(bus_ut, bus_baseline, bus_weights) 
+    neighbourhood.commutescores[1] = calculate_score(bus_delta, bus_baseline, bus_weights) 
 
     # bike: very mild impacts on convenience
-    bike_ut = neighbourhood.monthly_utilization[2] / population
+    bike_delta = neighbourhood.monthly_utilization[2] / population - 1/12
     bike_baseline =neighbourhood.baseline_scores[2]
     bike_weights = [0.1, 0, 0, 0]
-    neighbourhood.commutescores[2] = calculate_score(bike_ut, bike_baseline, bike_weights)
+    neighbourhood.commutescores[2] = calculate_score(bike_delta, bike_baseline, bike_weights)
 
     # walk: very mild impacts on convenience
-    walk_ut = neighbourhood.monthly_utilization[3] / population
+    walk_delta = neighbourhood.monthly_utilization[3] / population - 1/12
     walk_baseline = neighbourhood.baseline_scores[3]
     walk_weights = [0.05, 0, 0, 0]
-    neighbourhood.commutescores[3] = calculate_score(walk_ut, walk_baseline, walk_weights)
+    neighbourhood.commutescores[3] = calculate_score(walk_delta, walk_baseline, walk_weights)
 
-def calculate_score(utilization, baseline, weights):
+def calculate_score(deltas, baseline, weights):
 
     weights = np.array(weights)
     baseline = np.array(baseline)
-    
-
-    # exponential term for each element in the array
-    exp_term = np.exp(-weights * utilization)
+    deltas = np.array(deltas)
 
     # calculate the score for each element
-    scores = baseline + (10 - baseline) * exp_term
+    scores = baseline + weights * deltas
 
     # clip the scores
     scores = np.clip(scores, 0, 10)
+    print(scores)
 
     return scores
