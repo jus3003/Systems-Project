85dc8cc6b1742d8441b8dd00bcf885a46fc3d7fc
Jordan French <jordanfrench@utexas.edu>
2023-11-20 09:37:25 -0600

    updated congestion weights

--- agent_env_monthly.py
+++ agent_env_monthly.py
@@ -6,7 +6,7 @@ def commute_update(neighbourhood, population):
     
     # convenience updates on a monthly basis due to congestion
     for agent in neighbourhood.resident_list:
-        
+         
         neighbourhood.monthly_utilization[agent.transit] += 1
         
     # track utilization to inform annual investments
@@ -17,27 +17,27 @@ def commute_update(neighbourhood, population):
     # cars: all scores effected
     car_delta = neighbourhood.monthly_utilization[0] / population - 1/12
     car_baseline = neighbourhood.baseline_scores[0]
-    car_weights = [4, 5, 3, 3]
+    car_weights = [.1, .1, .05, .05]
 
     neighbourhood.commutescores[0] = calculate_score(car_delta, car_baseline, car_weights)
 
     # bus: mild impacts on convenience and speed
     bus_delta = neighbourhood.monthly_utilization[1] / population - 1/12
     bus_baseline = neighbourhood.baseline_scores[1]
-    bus_weights = [4, 3, 0, 0]
+    bus_weights = [.1, .05, 0, 0]
 
     neighbourhood.commutescores[1] = calculate_score(bus_delta, bus_baseline, bus_weights) 
 
     # bike: very mild impacts on convenience
     bike_delta = neighbourhood.monthly_utilization[2] / population - 1/12
     bike_baseline =neighbourhood.baseline_scores[2]
-    bike_weights = [1, 0, 0, 0]
+    bike_weights = [.02, 0, 0, 0]
     neighbourhood.commutescores[2] = calculate_score(bike_delta, bike_baseline, bike_weights)
 
     # walk: very mild impacts on convenience
     walk_delta = neighbourhood.monthly_utilization[3] / population - 1/12
     walk_baseline = neighbourhood.baseline_scores[3]
-    walk_weights = [1, 0, 0, 0]
+    walk_weights = [.02, 0, 0, 0]
     neighbourhood.commutescores[3] = calculate_score(walk_delta, walk_baseline, walk_weights)
 
 def calculate_score(deltas, baseline, weights):
